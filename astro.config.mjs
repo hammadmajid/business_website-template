16428f4c622cd1f923800c490ba5e0a101c7e0fb
Hammad Majid <hm.hammadmajid@gmail.com>
2022-10-20 06:00:42 +0000

    generate site-map on build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: 'https://ipsum-template.vercel.app',
+  integrations: [tailwind(), sitemap()],
   vite: {
     ssr: {
-      external: ['svgo']
-    }
-  }
-}); 
+      external: ["svgo"],
+    },
+  },
+});
