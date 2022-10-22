ad7131cc6166e3d33dbf1329cbb0e9f3b01e4027
hammadmajid <hm.hammadmajid@gmail.com>
2022-10-22 22:46:40 +0500

    Install and Config: astro/images and sharp

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,17 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import image from "@astrojs/image";
 
 export default defineConfig({
-  site: 'https://ipsum-template.vercel.app',
-  integrations: [tailwind(), sitemap()],
+  site: "https://ipsum-template.vercel.app",
+  integrations: [
+    tailwind(),
+    sitemap(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+  ],
   vite: {
     ssr: {
       external: ["svgo"],
